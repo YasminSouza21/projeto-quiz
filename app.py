5658927f56ff5af8e36c2a8f2d16ca0476467e73
yasmin-souza21 <YasminSouza21@users.noreply.github.com>
2025-09-04 09:40:47 -0300

    tela do quiz finalizada

--- app.py
+++ app.py
@@ -1,10 +1,75 @@
 import streamlit as st
-import time
+from google import genai
+from dotenv import load_dotenv
+import json
+import os
+import pandas as pd
 
-def jogar_quiz(tema, pagina):
+load_dotenv()
+api_key = os.getenv('API_KEY_GENAI')
+genai_client = genai.Client(api_key=api_key)
+
+def gerar_perguntas(tema): 
+    response = genai_client.models.generate_content(
+        model='gemini-1.5-flash',
+        contents=f'Faça 5 perguntas sobre esse tema {tema}, cada pergunta deve conter 4 alternativas e suas respectivas explicações, me de em json a reposta do prompt, no json deve conter a chave chamada pergunta, uma chave chamada respostas e uma chave chamada explicações com as 4 alternativas sem nomear elas de A a B ou de 1 a 4 e a chave chamada resposta_correta dizendo qual resposta a correta, mas quero a resposta correta em texto da resposta, não o index que a resposta ta, e traz um titulo sobre o tema do quiz tipo Quiz sobre ai vem o nome do tema e depois um emoji a chave pode ter nome de titulo'
+    ).to_json_dict() 
+    return str(response['candidates'][0]['content']['parts'][0]['text']).replace('```', '').replace('json', '')
+
+def jogar_quiz(perguntas, pagina):
     if pagina == 'quiz':
-        st.title(f'Quiz sobre {tema}', )
-
+        
+        json_perguntas = json.loads(perguntas)
+        
+        if 'quiz_iniciado' not in st.session_state:
+            st.session_state.quiz_iniciado = True
+        if 'numero_pergunta' not in st.session_state:
+            st.session_state.numero_pergunta = 0
+        if 'respondeu_pergunta' not in st.session_state:
+            st.session_state.respondeu_pergunta = False
+        if 'resposta_respondida' not in st.session_state:
+            st.session_state.resposta_respondida = ''
+        if 'index_resposta_respondida' not in st.session_state:
+            st.session_state.index_resposta_respondida = 0
+            
+        if st.session_state.quiz_iniciado:
+            st.set_page_config(layout='centered')
+            st.title(json_perguntas['titulo'])
+            with st.container(border=True, horizontal_alignment='center'):
+                st.markdown(f'## Pergunta {st.session_state.numero_pergunta + 1}/5')
+                st.markdown('### ' + json_perguntas['perguntas'][st.session_state.numero_pergunta]['pergunta'])
+                for index,resposta in enumerate(json_perguntas['perguntas'][st.session_state.numero_pergunta]['respostas']):
+                    if st.button(label=resposta, key=f'resposta_{index}', use_container_width=True, disabled=st.session_state.respondeu_pergunta):
+                        st.session_state.resposta_respondida = resposta
+                        st.session_state.respondeu_pergunta = True
+                        st.session_state.index_resposta_respondida = index
+                        st.rerun()
+                        
+                if st.session_state.respondeu_pergunta == True:
+                    resposta = st.session_state.resposta_respondida
+                    index = st.session_state.index_resposta_respondida
+                    if resposta == json_perguntas['perguntas'][st.session_state.numero_pergunta]['resposta_correta']:
+                                st.success(
+                                        f"🎉 Parabéns!!! Você acertou!\n\n"
+                                        f"💡 Explicação: {json_perguntas['perguntas'][st.session_state.numero_pergunta]['explicações'][index]}"
+                                )
+                    else:
+                        st.error(
+                            f"❌ Ops! Resposta errada.\n\n"
+                            f"✅ A resposta certa era **{json_perguntas['perguntas'][st.session_state.numero_pergunta]['resposta_correta']}**.\n\n"
+                            f"💡 Explicação: {json_perguntas['perguntas'][st.session_state.numero_pergunta]['explicações'][index]}"
+                        )  
+                        
+                    if st.session_state.respondeu_pergunta != False and st.session_state.numero_pergunta <= 3:
+                        if st.button(label='Próxima Pergunta', key=f'pergunta_{st.session_state.numero_pergunta}'):
+                            st.session_state.respondeu_pergunta = False
+                            st.session_state.numero_pergunta += 1
+                            st.rerun()
+                    elif st.session_state.respondeu_pergunta != False and st.session_state.numero_pergunta == 4:
+                        if st.button(label='Terminar quiz', key=f'btn_terminar_quiz_{st.session_state.numero_pergunta}'):
+                            st.session_state.quiz_iniciado = False
+                            st.rerun()
+            
 def iniciar_quiz():
     st.set_page_config(page_title='Quiz | Bee Smart', page_icon='🐝', layout='wide')
     
@@ -12,10 +77,13 @@ def iniciar_quiz():
         st.session_state.tema_escolhido = None
     if 'pagina_atual' not in st.session_state:
         st.session_state.pagina_atual = 'home'
+    if 'perguntas' not in st.session_state:
+        st.session_state.perguntas = None
     
     if st.session_state.pagina_atual == 'home':
         with st.container(vertical_alignment='center', horizontal_alignment='center'):
             st.markdown('# Seja bem-vindo ao Quiz :orange[Bee Smart] 🐝', width='content')
+            st.markdown('### O Quiz que irá te ajudar a testar seus conhecimentos com qualquer **TEMA**!!!')
             col_regra1, col_regra2, col_regra3 = st.columns(3, border=True, vertical_alignment='center')
             with col_regra1:
                 st.markdown('### Responda rápido e com atenção')
@@ -39,12 +107,13 @@ def iniciar_quiz():
 
             if st.session_state.tema_escolhido != None:
                 with st.spinner(text=f'Aguarde a IA gerar suas perguntas sobre o tema {st.session_state.tema_escolhido}'):
-                    time.sleep(10)  
-                    st.session_state.pagina_atual = 'quiz'
-                    st.rerun()
+                    st.session_state.perguntas = gerar_perguntas(st.session_state.tema_escolhido)
+                    if st.session_state.perguntas != None:
+                        st.session_state.pagina_atual = 'quiz'
+                        st.rerun()
                                                      
     elif st.session_state.pagina_atual == 'quiz':
-        jogar_quiz(st.session_state.tema_escolhido, st.session_state.pagina_atual)
+        jogar_quiz(st.session_state.perguntas, st.session_state.pagina_atual)
                 
 
                
